f3e6f8cc2bfc8caf775525e2e9cdad57689a61a8
Mihai Fufezan <fufexan@protonmail.com>
2022-08-22 20:29:55 +0300

    use flake inputs for files and repo

--- flake.nix
+++ flake.nix
@@ -1,12 +1,32 @@
 {
   description = "An anti-malware Web browser extension to mitigate potential threats from JavaScript, including fingerprinting, tracking, and data collection!";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    jshelter = {
+      url = "https://pagure.io/JShelter/webextension.git";
+      type = "git";
+      submodules = true;
+      # pagure won't expose refs
+      rev = "1d55d0bfd683560e63a139e6f957e587052728bd";
+      flake = false;
+    };
+    ipv4csv = {
+      url = "https://www.iana.org/assignments/locally-served-dns-zones/ipv4.csv";
+      flake = false;
+    };
+    ipv6csv = {
+      url = "https://www.iana.org/assignments/locally-served-dns-zones/ipv6.csv";
+      flake = false;
+    };
+  };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
+    jshelter,
+    ...
+  } @ inputs: let
     supportedSystems = ["x86_64-linux"];
     genSystems = nixpkgs.lib.genAttrs supportedSystems;
     pkgsFor = genSystems (system:
@@ -16,7 +36,11 @@
       });
   in {
     overlays.default = final: prev: {
-      jshelter = prev.callPackage ./jshelter.nix {};
+      jshelter = prev.callPackage ./jshelter.nix {
+        version = "0.11.1";
+        src = jshelter;
+        inherit (inputs) ipv4csv ipv6csv;
+      };
     };
 
     packages = genSystems (system: rec {
--- jshelter.nix
+++ jshelter.nix
@@ -7,83 +7,55 @@
   makeFontsConf,
   nodejs,
   zip,
-  fetchFromGitHub,
-  fetchurl,
-  fetchzip,
-}: let
-  pin = builtins.fromJSON (builtins.readFile ./pin.json);
+  ipv4csv,
+  ipv6csv,
+  src,
+  version,
+}:
+stdenv.mkDerivation {
+  pname = "jshelter";
+  inherit version src;
 
-  ipv4csv = fetchurl {
-    url = "https://www.iana.org/assignments/locally-served-dns-zones/ipv4.csv";
-    hash = "sha256-NYsxbG0WecF0+BO7hFlgmAACtDfL8P5PB556Z8iarms=";
+  nativeBuildInputs = [doxygen graphviz nodejs zip];
+
+  # Fontconfig error: Cannot load default config file
+  FONTCONFIG_FILE = makeFontsConf {
+    fontDirectories = [freefont_ttf];
   };
-  ipv6csv = fetchurl {
-    url = "https://www.iana.org/assignments/locally-served-dns-zones/ipv6.csv";
-    hash = "sha256-9ImunHzGKDZACTknDe86EwUHZv2DS1g9U+ofDC3Pa20=";
+
+  # we get submodules and CSV using FOD
+  postPatch = ''
+    patchShebangs fix_manifest.sh generate_fpd.sh nscl/include.sh
+    substituteInPlace Makefile \
+      --replace '$(COMMON_FILES) get_csv submodules' '$(COMMON_FILES)'
+  '';
+
+  preBuild = ''
+    cp ${ipv4csv} common/ipv4.dat
+    cp ${ipv6csv} common/ipv6.dat
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/jshelter
+    cp jshelter_{chrome,firefox}.zip $out/share/jshelter/
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "An anti-malware Web browser extension to mitigate potential threats from JavaScript, including fingerprinting, tracking, and data collection";
+    homepage = "https://jshelter.org";
+    license = with licenses; [
+      cc0
+      cc-by-40
+      fdl13Plus
+      gpl3Plus
+      mit
+      mpl20
+    ]; # + CC-BY-ND-4.0
+    platforms = platforms.unix;
+    maintainers = with maintainers; [fufexan];
   };
-  nscl = fetchFromGitHub {
-    owner = "hackademix";
-    repo = "nscl";
-    rev = pin.nsclRev;
-    sha256 = pin.nsclHash;
-  };
-in
-  stdenv.mkDerivation rec {
-    pname = "jshelter";
-    version = pin.version;
-
-    src = fetchzip {
-      url = "https://pagure.io/JShelter/webextension/archive/${version}/webextension-${version}.zip";
-      sha256 = pin.hash;
-    };
-
-    nativeBuildInputs = [doxygen graphviz nodejs zip];
-
-    # Fontconfig error: Cannot load default config file
-    FONTCONFIG_FILE = makeFontsConf {
-      fontDirectories = [freefont_ttf];
-    };
-
-    # we copy instead of link as sed will try to write temp files in there
-    postUnpack = ''
-      rm -rf source/nscl
-      cp -r ${nscl} source/nscl
-      chmod -R +w source/nscl
-    '';
-
-    # we get submodules and CSV using FOD
-    postPatch = ''
-      patchShebangs fix_manifest.sh generate_fpd.sh nscl/include.sh
-      substituteInPlace Makefile \
-        --replace '$(COMMON_FILES) get_csv submodules' '$(COMMON_FILES)'
-    '';
-
-    preBuild = ''
-      cp ${ipv4csv} common/ipv4.dat
-      cp ${ipv6csv} common/ipv6.dat
-    '';
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/share/jshelter
-      cp jshelter_{chrome,firefox}.zip $out/share/jshelter/
-
-      runHook postInstall
-    '';
-
-    meta = with lib; {
-      description = "An anti-malware Web browser extension to mitigate potential threats from JavaScript, including fingerprinting, tracking, and data collection";
-      homepage = "https://jshelter.org";
-      license = with licenses; [
-        cc0
-        cc-by-40
-        fdl13Plus
-        gpl3Plus
-        mit
-        mpl20
-      ]; # + CC-BY-ND-4.0
-      platforms = platforms.unix;
-      maintainers = with maintainers; [fufexan];
-    };
-  }
+}
